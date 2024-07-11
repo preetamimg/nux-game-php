5681ca1b92ee461e33f4113ea1add7f36c8d6ad7
Ratan Kasnia <ratankasnia.img@gmail.com>
2024-07-11 11:33:55 +0530

    blog inner

---
+++ blogInner/index.php
@@ -0,0 +1,25 @@
+<?php include '../layouts/header-link.php' ?>
+<?php include '../layouts/header.php' ?>
+<main>
+
+
+
+
+    <div class="main_wrapper newSinglePage newSingleProductPage">
+
+
+    
+    </div>
+
+
+
+
+
+    <?php include '../layouts/footer.php' ?>
+    <?php include '../layouts/footer-link.php' ?>
+</main>
+<script>
+    $(function () {
+        $('.header').addClass('Header_inner');
+    });
+</script>
