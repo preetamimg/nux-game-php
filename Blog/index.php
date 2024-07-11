39d611414e394021532225146e79d79f1788d42b
mohitboharaimg <146337834+mohitboharaimg@users.noreply.github.com>
2024-07-11 11:29:17 +0530

    done

---
+++ Blog/index.php
@@ -0,0 +1,202 @@
+<?php include '../layouts/header-link.php' ?>
+<?php include '../layouts/header.php' ?>
+
+<main>
+<div class="main_wrapper newSinglePage">
+    <section class="topHeader paddingTopSection">
+      <div class="container">
+        <div class="breadCrumbsWrapper">
+          <ul class="breadCrumbs" itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="active" href="index.html" itemprop="item">
+                <span itemprop="name">Home</span></a>
+              <meta itemprop="position" content="1" />
+            </li>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <span class="bread" itemprop="name">Blog</span>
+              <meta itemprop="position" content="2" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <div class="blogPageMain">
+      <div class="container">
+        <div class="blogWrapper">
+          <div class="mainArticleWrapper">
+            <div class="mainArticleImageWrapper">
+              <picture>
+                <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=692&h=532&fit=crop&fm=webp"
+                  type="image/webp">
+                <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-1b830.png?w=692&amp;h=532&amp;fit=crop"
+                  title="NuxGame Teams Up with Changelly for Seamless Crypto Exchanges" />
+              </picture>
+            </div>
+            <div class="mainArticleTextWrapper">
+              <div class="category">
+                Platform Updates
+              </div>
+              <h2 class="darkPurple">NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              <div class="datePublish">
+                June 27, 2024
+              </div>
+              <p class="description">
+                NuxGame partners with Changelly for seamless fiat-to-crypto exchanges, enhancing user experience and
+                payment flexibility.
+              </p>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="btnBlogMore">
+                Load more <img src="img/icons/more_blog.svg" />
+              </a>
+            </div>
+            <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+              class="mainArticleLink"></a>
+          </div>
+          <div class="tagWrapper">
+            <ul class="tagList tabsList">
+              <li class="active">
+                <div><span>All</span></div>
+              </li>
+              <li class=" ">
+                <a href="javascript:;">
+                  <span>Platform Updates</span>
+                </a>
+              </li>
+              <li class=" ">
+                <a href="javascript:;">
+                  <span>Industry Insights</span>
+                </a>
+              </li>
+              <li class=" ">
+                <a href="javascript:;">
+                  <span>Providers</span>
+                </a>
+              </li>
+              <li class=" ">
+                <a href="javascript:;">
+                  <span>Legal Hub</span>
+                </a>
+              </li>
+              <li class=" ">
+                <a href="javascript:;">
+                  <span>Guides</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div class="blogListWrapper" id="blogList">
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-13d90.png?w=590&amp;h=288&amp;fit=crop" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges.html"
+                class="linkOverlay"> </a>
+            </div>
+          </div>
+          <a id="view_more_btn" href="blog4658.html?page=2" nochange class="btnBlogMore">View more <img
+              src="<?php echo f_path ?>img/icons/more_blog.svg" /></a>
+        </div>
+      </div>
+    </div>
+  </div>
+    
+
+
+    <?php include '../layouts/footer.php' ?>
+    <?php include '../layouts/footer-link.php' ?>
+</main>
