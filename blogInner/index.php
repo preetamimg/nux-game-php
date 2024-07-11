822301649f00cbe1d598ccc65eca74ca836ce07b
Ratan Kasnia <ratankasnia.img@gmail.com>
2024-07-11 11:47:06 +0530

    blog inner done

--- blogInner/index.php
+++ blogInner/index.php
@@ -1,25 +1,308 @@
 <?php include '../layouts/header-link.php' ?>
 <?php include '../layouts/header.php' ?>
+
 <main>
-
-
-
-
-    <div class="main_wrapper newSinglePage newSingleProductPage">
-
-
-    
+<div class="main_wrapper newSinglePage">
+   
+<section class="topHeader paddingTopSection">
+      <div class="container">
+        <div class="breadCrumbsWrapper">
+          <ul class="breadCrumbs" itemscope itemtype="https://schema.org/BreadcrumbList">
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a class="active" href="../../index.html" itemprop="item">
+                <span itemprop="name">Home</span></a>
+              <meta itemprop="position" content="1" />
+            </li>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a itemprop="item" href="../../blog.html">
+                <span itemprop="name">Blog</span>
+              </a>
+              <meta itemprop="position" content="2" />
+            </li>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <a itemprop="item" href="../../blog.html">
+                <span itemprop="name">Platform Updates</span>
+              </a>
+              <meta itemprop="position" content="3" />
+            </li>
+            <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+              <span class="bread" itemprop="name">NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</span>
+              <meta itemprop="position" content="4" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+    <div class="blogPageMain blogArticle">
+      <div class="container">
+        <div class="article">
+          <article class="articleWrapper" itemscope itemtype="http://schema.org/Article">
+            <meta itemprop="headline" content="NuxGame Teams Up with Changelly for Seamless Crypto Exchanges" />
+            <meta itemscope itemprop="mainEntityOfPage" itemType="https://schema.org/WebPage"
+              itemid="https://nuxgame.com/blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges" />
+            <meta itemprop="url"
+              content="https://nuxgame.com/blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges" />
+            <meta itemprop="datePublished" content="2024-06-27 11:41:00" />
+            <meta itemprop="dateModified" content="2024-06-27 11:47:54" />
+            <meta itemprop="identifier"
+              content="https://nuxgame.com/blog/platform-updates/nuxgame-teams-up-with-changelly-seamless-crypto-exchanges" />
+            <meta itemprop="about" content="All-in-One Platform" />
+            <meta itemprop="about" content="Global iGaming" />
+            <meta itemprop="about" content="Crypto" />
+            <div class="articleHeading">
+              <div class="category">
+                Platform Updates
+              </div>
+              <div class="datePublish">
+                June 27, 2024
+              </div>
+              <h1 class="title">NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h1>
+              <picture itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+                <source srcset="<?php echo f_path ?>img/glide/@public/_article/Blog 1200_600-1.png?fm=webp" type="image/webp">
+                <img itemprop="image url" src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-165b7.png?fm=png"
+                  class="imgWrapperImage" alt="changelly-partnership" title="changelly-partnership" />
+              </picture>
+            </div>
+            <div class="contentWrapper">
+              <section class="articleBody" itemprop="articleBody">
+                <div id="anc_intro" class="articleBodyText">
+                  <p>We&#39;re excited to announce that <a href="../../index.html">NuxGame</a>, your trusted iGaming
+                    software provider, has joined forces with Changelly to integrate their fiat-to-crypto exchange
+                    service.&nbsp;</p>
+                  <p>Changelly&rsquo;s software supports fiat-to-crypto exchanges for over 250 currencies, making the
+                    whole process smoother for both players and operators. This integration lets players top up their
+                    accounts with cryptocurrency even if they&rsquo;ve never owned any before. With support for over 80
+                    worldwide fiat currencies, transactions are now more streamlined.</p>
+                  <p>This partnership showcases NuxGame&#39;s dedication to improving the user experience in payments
+                    and ensuring transparent transactions. By integrating different payment processors into our
+                    platform, we&#39;ve expanded our payment functionality, allowing players to purchase and exchange
+                    cryptocurrency. This move offers a broad range of scalable solutions to meet diverse market needs.
+                  </p>
+                  <p>This latest integration expands NuxGame payment functionality as it continues to bolster our
+                    platform and upgrade service.<br />
+                    &nbsp;</p>
+                </div>
+                <blockquote class="blockquote">
+                  <div class="blockquoteImage">
+                    <img src="<?php echo f_path ?>img/glide/@public/dmitriy-volkov.webp" />
+                  </div>
+                  <div class="blockquoteTextWrapper">
+                    <div class="blockquoteText">
+                      <p>Dmitry Volkov, CRO at NuxGame, said: &ldquo;This partnership marks our continuous efforts to
+                        improve the NuxGame platform. By integrating Changelly&#39;s fiat-to-crypto solution, we are
+                        providing our clients with greater flexibility and more options for operating their businesses.
+                        These steps reaffirm our commitment to provide our clients with the best solutions in the
+                        market.&quot;<br />
+                        &nbsp;</p>
+                    </div>
+                  </div>
+                </blockquote>
+                <div id="anc_2" class="articleBodyText">
+                  <p>Stay tuned for more updates as we continue to innovate and enhance your gaming experience with
+                    NuxGame!</p>
+                </div>
+              </section>
+              <aside class="contentsInfo">
+                <div class="authorArticle">
+                  <span class="authorImageArticle" itemprop="publisher" itemscope
+                    itemtype="http://schema.org/Organization">
+                    <meta itemprop="name" content="Nuxgame" />
+                    <picture itemprop="logo" itemscope="" itemtype="https://schema.org/ImageObject">
+                      <source srcset="https://nuxgame.com/glide/@public/Blog/halyna.jpeg?w=50&h=50&fit=crop&fm=webp"
+                        type="image/webp">
+                      <img itemprop="image url"
+                        src="https://nuxgame.com/glide/@public/Blog/halyna.jpeg?w=50&h=50&fit=crop&fm=webp"
+                        alt="Halyna">
+                    </picture>
+                  </span>
+                  <div class="authorNameArticle">
+                    <span itemprop="author" itemscope itemtype="http://schema.org/Person">
+                      <span class="authorNameArticleTitle" itemprop="name">
+                        Halyna
+                      </span>
+                    </span>
+                    <span>
+                      <p>NuxGame Editorial<br></p>
+                    </span>
+                  </div>
+                </div>
+                <div class="contentInfoNav">
+                  <div class="contentInfoTitle">TABLE OF CONTENTS</div>
+                  <div class="contentInfoList">
+                    <ol>
+                      <li>
+                        <a href="#anc_intro" onclick="scrollToAnchorBlock(this)">
+                          Expanded our payment functionality
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#anc_2" onclick="scrollToAnchorBlock(this)">
+                          Stay tuned to know our next improvements
+                        </a>
+                      </li>
+                    </ol>
+                  </div>
+                </div>
+                <div class="contentsInfoLinks">
+                  <div class="socialWrapper">
+                    <p>Find us in social media</p>
+                    <h4>FOLLOW US</h4>
+                    <ul class="social">
+                      <li>
+                        <a href="https://www.instagram.com/nuxgame/" aria-label="instagram" target="_blank">
+                          <div class="socialIconWrapper">
+                            <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path
+                                d="M9.75 0H3.25C1.46266 0 0 1.46202 0 3.25V9.75C0 11.5373 1.46266 13 3.25 13H9.75C11.5373 13 13 11.5373 13 9.75V3.25C13 1.46202 11.5373 0 9.75 0ZM6.5 9.20823C5.00402 9.20823 3.79161 7.99532 3.79161 6.5C3.79161 5.00402 5.00402 3.79161 6.5 3.79161C7.99532 3.79161 9.20839 5.00402 9.20839 6.5C9.20839 7.99532 7.99532 9.20823 6.5 9.20823ZM10.0209 3.79161C9.57173 3.79161 9.20839 3.42777 9.20839 2.97911C9.20839 2.53046 9.57173 2.16661 10.0209 2.16661C10.47 2.16661 10.8334 2.53046 10.8334 2.97911C10.8334 3.42777 10.47 3.79161 10.0209 3.79161Z"
+                                fill="#060048" />
+                            </svg>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.facebook.com/nuxgame/" aria-label="facebook" target="_blank">
+                          <div class="socialIconWrapper">
+                            <svg width="7" height="13" viewBox="0 0 7 13" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path
+                                d="M3.97785 12.858V6.99317H5.78596L6.05582 4.70717H3.97785V3.24977C3.97785 2.58571 4.14516 2.13909 5.01953 2.13909H6.13139V0.0881492C5.93708 0.0587661 5.27861 0 4.51218 0C2.90917 0 1.81351 1.06367 1.81351 3.02058V4.70717H0V6.99317H1.81351V12.858H3.97785Z"
+                                fill="white" />
+                            </svg>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.youtube.com/@Nuxgame" aria-label="youtube" target="_blank">
+                          <div class="socialIconWrapper">
+                            <svg width="17" height="11" viewBox="0 0 17 11" class="youtubeIcon" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M16.6444 1.71753C16.4489 1.04158 15.873 0.508753 15.1413 0.327892C13.816 1.18977e-07 8.5 0 8.5 0C8.5 0 3.18396 1.18977e-07 1.85796 0.327892C1.12696 0.508753 0.551083 1.04093 0.355583 1.71753C0 2.94322 0 5.5 0 5.5C0 5.5 0 8.05678 0.355583 9.28247C0.551083 9.95842 1.12696 10.4912 1.85867 10.6721C3.18396 11 8.5 11 8.5 11C8.5 11 13.816 11 15.142 10.6721C15.873 10.4912 16.4489 9.95907 16.6451 9.28247C17 8.05678 17 5.5 17 5.5C17 5.5 17 2.94322 16.6444 1.71753ZM6.76175 7.82192V3.17808L11.2044 5.5L6.76175 7.82192Z"
+                                fill="white" />
+                            </svg>
+                          </div>
+                        </a>
+                      </li>
+                      <li>
+                        <a href="https://www.linkedin.com/company/nuxgame/" aria-label="linkedin" target="_blank">
+                          <div class="socialIconWrapper">
+                            <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
+                              xmlns="http://www.w3.org/2000/svg">
+                              <path
+                                d="M2.77049 13H0.213115V3.94708H2.77049V13ZM1.4918 2.90352C0.668328 2.90352 0 2.25336 0 1.45176C0 0.650164 0.668328 0 1.4918 0C2.31528 0 2.98361 0.650164 2.98361 1.45176C2.98361 2.25336 2.31613 2.90352 1.4918 2.90352ZM13 13H10.4426V8.38795C10.4426 5.6161 7.03279 5.82597 7.03279 8.38795V13H4.47541V3.94708H7.03279V5.39966C8.22282 3.2714 13 3.11421 13 7.43739V13Z"
+                                fill="white" />
+                            </svg>
+                          </div>
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </aside>
+            </div>
+          </article>
+        </div>
+      </div>
+      <section class="relatedArticles">
+        <div class="container">
+          <h2 class="darkPurple">Related Articles</h2>
+          <div class="blogListWrapper">
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="<?php echo f_path ?>img/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-165b7.png?fm=png" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="javascript:;" class="linkOverlay"> </a>
+            </div>
+            <div class="blogItemWrapper">
+              <div class="blogImageWrapper">
+                <picture>
+                  <source srcset="<?php echo f_path ?>img/glide/@public/_article/Blog 1200_600-1.png?w=590&h=288&fit=crop&fm=webp"
+                    type="image/webp">
+                  <img src="<?php echo f_path ?>img/glide/@public/_article/Blog%201200_600-165b7.png?fm=png" />
+                </picture>
+              </div>
+              <div class="blogTextWrapper">
+                <div class="category">
+                  Platform Updates
+                </div>
+                <h2>NuxGame Teams Up with Changelly for Seamless Crypto Exchanges</h2>
+              </div>
+              <a href="javascript:;" class="linkOverlay"> </a>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="container">
+        <div class="contentsInfoLinksMobile">
+          <div class="socialWrapper">
+            <p>Find us in social media</p>
+            <h4>FOLLOW US</h4>
+            <ul class="social">
+              <li>
+                <a href="https://www.instagram.com/nuxgame/" aria-label="instagram" target="_blank">
+                  <div class="socialIconWrapper">
+                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M9.75 0H3.25C1.46266 0 0 1.46202 0 3.25V9.75C0 11.5373 1.46266 13 3.25 13H9.75C11.5373 13 13 11.5373 13 9.75V3.25C13 1.46202 11.5373 0 9.75 0ZM6.5 9.20823C5.00402 9.20823 3.79161 7.99532 3.79161 6.5C3.79161 5.00402 5.00402 3.79161 6.5 3.79161C7.99532 3.79161 9.20839 5.00402 9.20839 6.5C9.20839 7.99532 7.99532 9.20823 6.5 9.20823ZM10.0209 3.79161C9.57173 3.79161 9.20839 3.42777 9.20839 2.97911C9.20839 2.53046 9.57173 2.16661 10.0209 2.16661C10.47 2.16661 10.8334 2.53046 10.8334 2.97911C10.8334 3.42777 10.47 3.79161 10.0209 3.79161Z"
+                        fill="#060048" />
+                    </svg>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/nuxgame/" aria-label="facebook" target="_blank">
+                  <div class="socialIconWrapper">
+                    <svg width="7" height="13" viewBox="0 0 7 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M3.97785 12.858V6.99317H5.78596L6.05582 4.70717H3.97785V3.24977C3.97785 2.58571 4.14516 2.13909 5.01953 2.13909H6.13139V0.0881492C5.93708 0.0587661 5.27861 0 4.51218 0C2.90917 0 1.81351 1.06367 1.81351 3.02058V4.70717H0V6.99317H1.81351V12.858H3.97785Z"
+                        fill="white" />
+                    </svg>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/@Nuxgame" aria-label="youtube" target="_blank">
+                  <div class="socialIconWrapper">
+                    <svg width="17" height="11" viewBox="0 0 17 11" class="youtubeIcon" fill="none"
+                      xmlns="http://www.w3.org/2000/svg">
+                      <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M16.6444 1.71753C16.4489 1.04158 15.873 0.508753 15.1413 0.327892C13.816 1.18977e-07 8.5 0 8.5 0C8.5 0 3.18396 1.18977e-07 1.85796 0.327892C1.12696 0.508753 0.551083 1.04093 0.355583 1.71753C0 2.94322 0 5.5 0 5.5C0 5.5 0 8.05678 0.355583 9.28247C0.551083 9.95842 1.12696 10.4912 1.85867 10.6721C3.18396 11 8.5 11 8.5 11C8.5 11 13.816 11 15.142 10.6721C15.873 10.4912 16.4489 9.95907 16.6451 9.28247C17 8.05678 17 5.5 17 5.5C17 5.5 17 2.94322 16.6444 1.71753ZM6.76175 7.82192V3.17808L11.2044 5.5L6.76175 7.82192Z"
+                        fill="white" />
+                    </svg>
+                  </div>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/company/nuxgame/" aria-label="linkedin" target="_blank">
+                  <div class="socialIconWrapper">
+                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path
+                        d="M2.77049 13H0.213115V3.94708H2.77049V13ZM1.4918 2.90352C0.668328 2.90352 0 2.25336 0 1.45176C0 0.650164 0.668328 0 1.4918 0C2.31528 0 2.98361 0.650164 2.98361 1.45176C2.98361 2.25336 2.31613 2.90352 1.4918 2.90352ZM13 13H10.4426V8.38795C10.4426 5.6161 7.03279 5.82597 7.03279 8.38795V13H4.47541V3.94708H7.03279V5.39966C8.22282 3.2714 13 3.11421 13 7.43739V13Z"
+                        fill="white" />
+                    </svg>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-
-
-
+  </div>
+    
 
 
     <?php include '../layouts/footer.php' ?>
     <?php include '../layouts/footer-link.php' ?>
 </main>
-<script>
-    $(function () {
-        $('.header').addClass('Header_inner');
-    });
-</script>
